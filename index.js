6b22f9d4cad34f8ec591dfb3cd0e6508344c3f0f
laurealim <laureal.seu@gmail.com>
2019-06-19 23:47:03 +0600

    nedb add to the project and data add to the db.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+var Datastore = require("nedb");
+
 const app = express();
 const fs = require("fs");
 
@@ -9,42 +11,16 @@ app.listen(3000, () => {
 app.use(express.static("public"));
 app.use(express.json({ limit: "1mb" }));
 
+const database = new Datastore("database.db");
+database.loadDatabase();
+
+// database.insert({ name: "laureal", status: 1 });
+
 app.post("/api", (req, res) => {
-  //   console.log(req.body);
-
-  //   fs.stat.isFile("temp.txt", (err, stat) => {
-  //   console.log('chek');
-
-  // if (err == null) {
-  //   console.log("File exists");
-  //   fs.appendFile("temp.txt", "Hello content!", err => {
-  //     if (err) throw err;
-  //     console.log("Saved!");
-  //   });
-  // } else if (err.code === "ENOENT") {
-  //   // file does not exist
-  //   fs.writeFile("temp.txt", "Some log\n");
-  // } else {
-  //   console.log("Some other error: ", err.code);
-  // }
-  //   });
-  const saveData = {
-    status: "success",
-    latitude: req.body.lat,
-    longitude: req.body.lon,
-    timestamp: req.body.time
-  };
-  console.log(saveData);
-
-  fs.appendFile("temp.txt", JSON.stringify(saveData)+'\n', function(err) {
-    if (err) throw err;
-    console.log("Saved!");
-  });
-
-  res.json({
-    status: "success",
-    lat: req.body.lat,
-    lon: req.body.lon,
-    timestamp: req.body.time
-  });
+  const data = req.body;
+  const serverTime = Date.now();
+  data.serverTime = serverTime;
+  console.log(data);
+  database.insert(data);
+  res.json(data);
 });
